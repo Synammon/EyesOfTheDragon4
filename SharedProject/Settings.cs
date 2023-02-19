a9ad5d4336cc81c56dbb59467f5c62c80b0e1588
Synammon <cynth.mcmahon@gmail.com>
2023-02-19 11:20:21 -0500

    Adding conversation components, part 1

--- SharedProject/Settings.cs
+++ SharedProject/Settings.cs
@@ -12,11 +12,15 @@ namespace SharedProject
 
         public static Rectangle BaseRectangle { get { return new(0, 0, BaseWidth, BaseHeight); } }
         public static Rectangle TargetRectangle { get { return new(0, 0, TargetWidth, TargetHeight); } }
+        public static Point Resolution { get; set; } = new(TargetWidth, TargetHeight);
         public static int TargetWidth { get; set; } = BaseWidth;
         public static int TargetHeight { get; set; } = BaseHeight;
+
         public static Vector2 Scale 
         { 
             get { return new((float)TargetWidth / BaseWidth, (float)TargetHeight / BaseHeight);}         
         }
+
+
     }
 }
